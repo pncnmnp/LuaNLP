0f4c87b6a2dd2af144c9a23f7b2c7c004a8aaa3d
parthparikh <parthparikh1999p@gmail.com>
2021-02-22 20:09:00 +0530

    Modified tokenization to perform as module

--- tokenizer/tokenization.lua
+++ tokenizer/tokenization.lua
@@ -5,7 +5,9 @@ local rex = require "rex_pcre"
     https://nlp.stanford.edu/IR-book/html/htmledition/tokenization-1.html
 --]]
 
-function regex_tokenize(input)
+local Tokenize = {}
+
+function Tokenize.regex_tokenize(input)
     --[[ Algorithm ported from Jurafsky and Martin
     Edition 3, Chapter 2, Page 16 - Figure 2.12
 
@@ -22,19 +24,19 @@ function regex_tokenize(input)
     return tokens
 end
 
-function whitespace_tokenize(input)
+function Tokenize.whitespace_tokenize(input)
     -- Tokenizes on whitespaces
     local tokens = rex.gmatch(input, "\\S+")
     return tokens
 end
 
-function character_tokenize(input)
+function Tokenize.character_tokenize(input)
     -- Tokenizes on characters
     local tokens = rex.gmatch(input, ".")
     return tokens
 end
 
-function sentence_tokenize(input)
+function Tokenize.sentence_tokenize(input)
     --[[ Tokenizes sentences by detecting sentence boundaries using orthographic information as primary evidence
 
     For detecting edge cases, the regular expression pattern was tested on https://github.com/diasks2/pragmatic_segmenter#the-golden-rules
@@ -70,7 +72,7 @@ function sentence_tokenize(input)
     return tokens
 end
 
-function test()
+local function test()
     local str1 = "what're who doesn't like to play football. you've been x punk'd hahaha coud've would've should've raj! you'll what're be late again? ridiculous! don't they'll what're you doing here we're "
     local str2 = "That U.S.A. poster-print costs $12.40 ... or 82% or 99.99% or even 12.40 even Wall-E would love it especially after A.B.C. purchased the product but who doesn't love wall-e! We all love him! Somebody knocked on the door. It must be Mr. White. He is the one who knocks (Breaking Bad reference). He [2019]{cheese} Agai\"\n 3+4/2-5*2 /|\\ or 3 % or M#Ash 22/10/10 is my birthday. 3+2+1 is the equation we are looking for! abc@gmail.com is my email id"
     local str3 = "Sentence boundary disambiguation (SBD), also known as sentence breaking, sentence boundary detection, and sentence segmentation, is the problem in natural language processing of deciding where sentences begin and end. Natural language processing tools often require their input to be divided into sentences; however, sentence boundary identification can be challenging due to the potential ambiguity of punctuation marks. In written English, a period may indicate the end of a sentence, or may denote an abbreviation, a decimal point, an ellipsis, or an email address, among other possibilities. About 47% of the periods in the Wall Street Journal corpus denote abbreviations.[1] Question marks and exclamation marks can be similarly ambiguous due to use in emoticons, computer code, and slang. St. Michael's Church is on 5th st. near the light. I can see Mt. Fuji from here. That is JFK Jr.'s book."
@@ -80,7 +82,7 @@ function test()
 
     local str5="These are the list items- 1) Hello world 2. bye bye"
     local val = "<START>"
-    local patt = regex_tokenize(str2)
+    local patt = sentence_tokenize(str6)
     while val ~= nil do
         io.write(val .. "|")
         val = patt()
@@ -88,4 +90,6 @@ function test()
     print("<END>")
 end
 
-test()
+-- test()
+
+return Tokenize
